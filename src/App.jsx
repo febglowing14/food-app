961e74a79db15330fd218ac8c0439f1f90c85443
febglowing14 <gopikavijayarajan@gmail.com>
2025-01-19 21:40:49 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import Header from './components/Header.jsx';
 import Main from './components/Main.jsx';
-import {BrowserRouter, Router, Route} from 'react-router-dom'
+import Login from './components/Login.jsx';
+import { BrowserRouter, Routes, Route,Link } from 'react-router-dom'
 
 export default function App() {
-    return (
-      <>
+  return (
+    <>
       <BrowserRouter>
-      <Router>
-        <Route path='/'element={<Header/>} />
-        <Route path='/about' element={<Main/>}/>
-
-      
-      </Router>
+      <ul>
+        <li><Link to ='/login'>Login</Link></li>
+      </ul>
+        <Routes>
+          <Route path='/' element={<Header />} />
+          <Route path='/about' element={<Main />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
       </BrowserRouter>
-       <Header />
-       <Main />
-       </>
-    )
+      {/* <Header /> */}
+      {/* <Main /> */}
+    </>
+  )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,40 +2,48 @@ import BreakFast from '../images/Breakfast.jpg';
 import Lunch from '../images/Lunch.jpg';
 import Dinner from '../images/Dinner.jpg';
 import shake from '../images/shake.jpg';
-import ice  from '../images/ice.jpg';
+import ice from '../images/ice.jpg';
+import { useNavigate } from 'react-router-dom';
+import Login from './Login';
+
 
 export default function Header() {
+    const navigate = useNavigate();
+
+    function handleLogin() {
+        navigate('/login')
+    }
     return (
         <div >
             <div className='logins'>
-        <h1>Food delivery app!</h1>
-        <input className='search' placeholder='search'></input>
-        <div className='menu'>
-        <h4>Singnup</h4>
-        <h4>signin</h4>
-        <h4>cart</h4>
-        <h4>login</h4>
-        </div>
-        </div>
-        <div className='header'>
-        <img className='one' src={BreakFast} alt="Breakfast" />
-        
-        <img className='two'src={Lunch} alt='Lunch'/>
-        
-        <img className='three' src={Dinner} alt='Dinner'/>
-        <img className='four' src={shake} alt='shake'/>
-        <img className='five' src={ice} alt='ice'/>
+                <h1>Food delivery app!</h1>
+                <input className='search' placeholder='search'></input>
+                <div className='menu'>
+                    <h4>Singnup</h4>
+                    <h4>signin</h4>
+                    <h4>cart</h4>
+                    <button className='login' onClick={handleLogin}> Login</button>
+                </div>
+            </div>
+            <div className='header'>
+                <img className='one' src={BreakFast} alt="Breakfast" />
 
-        
-       
-        </div>
-        <div className='list'>
-        <h4>BreakFast</h4>
-        <h4>Lunch</h4>
-        <h4>Dinner</h4>
-        <h4>shake</h4>
-        <h4>ice</h4>
-        </div>
+                <img className='two' src={Lunch} alt='Lunch' />
+
+                <img className='three' src={Dinner} alt='Dinner' />
+                <img className='four' src={shake} alt='shake' />
+                <img className='five' src={ice} alt='ice' />
+
+
+
+            </div>
+            <div className='list'>
+                <h4>BreakFast</h4>
+                <h4>Lunch</h4>
+                <h4>Dinner</h4>
+                <h4>shake</h4>
+                <h4>ice</h4>
+            </div>
         </div>
     )
-    }
+}
